2df513096f45c573c7d7d0748119135d8d1ca94f
Matthew Dennis <mdennis660@icloud.com>
2025-12-26 20:42:29 -0800

    Initial Commit

---
+++ SoundboardTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundboardTemplate
+//
+//  Created by Matthew Dennis on 12/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundboardTemplate/SoundboardTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundboardTemplateApp.swift
+//  SoundboardTemplate
+//
+//  Created by Matthew Dennis on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundboardTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
